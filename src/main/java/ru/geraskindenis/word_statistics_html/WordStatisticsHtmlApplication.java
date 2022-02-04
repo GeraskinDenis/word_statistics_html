174acd91d27c12cf3bfe978d63ca541963373dff
Geraskin Denis <fresh7tea@gmail.com>
2022-02-04 17:37:58 +0300

    Added WordStatisticsHtmlApplication

---
+++ src/main/java/ru/geraskindenis/word_statistics_html/WordStatisticsHtmlApplication.java
@@ -0,0 +1,13 @@
+package ru.geraskindenis.word_statistics_html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordStatisticsHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordStatisticsHtmlApplication.class, args);
+    }
+
+}
