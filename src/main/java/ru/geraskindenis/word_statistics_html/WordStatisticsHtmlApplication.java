25f3ec551c060d7716d4904f1b0bbaa5e496a1cf
Geraskin Denis <fresh7tea@gmail.com>
2022-02-04 19:15:15 +0300

    Class WordStatisticsHtmlApplication

--- src/main/java/ru/geraskindenis/word_statistics_html/WordStatisticsHtmlApplication.java
+++ src/main/java/ru/geraskindenis/word_statistics_html/WordStatisticsHtmlApplication.java
@@ -1,13 +1,27 @@
 package ru.geraskindenis.word_statistics_html;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import ru.geraskindenis.word_statistics_html.service.InitService;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class WordStatisticsHtmlApplication {
 
+    private static String url = "";
+
+    private final InitService initService;
+
     public static void main(String[] args) {
+        url = args.length == 0 ? "https://www.simbirsoft.com/": args[0];
         SpringApplication.run(WordStatisticsHtmlApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        initService.run(url);
+    }
 }
