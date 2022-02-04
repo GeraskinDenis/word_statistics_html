53501195dae3a39f605243b043583d19216d7c1c
Geraskin Denis <fresh7tea@gmail.com>
2022-02-04 19:36:04 +0300

    Added logging

--- src/main/java/ru/geraskindenis/word_statistics_html/WordStatisticsHtmlApplication.java
+++ src/main/java/ru/geraskindenis/word_statistics_html/WordStatisticsHtmlApplication.java
@@ -1,27 +1,32 @@
 package ru.geraskindenis.word_statistics_html;
 
+<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+=======
+>>>>>>> parent of 25f3ec5 (Class WordStatisticsHtmlApplication)
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import ru.geraskindenis.word_statistics_html.service.InitService;
 
 @SpringBootApplication
+<<<<<<< HEAD
 @RequiredArgsConstructor
+@Slf4j
+=======
+>>>>>>> parent of 25f3ec5 (Class WordStatisticsHtmlApplication)
 public class WordStatisticsHtmlApplication {
 
-    private static String url = "";
-
-    private final InitService initService;
-
     public static void main(String[] args) {
-        url = args.length == 0 ? "https://www.simbirsoft.com/": args[0];
         SpringApplication.run(WordStatisticsHtmlApplication.class, args);
+        log.info("Starting application with {} arguments.", args.length);
     }
 
+<<<<<<< HEAD
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
+        log.info("Before Starting Service");
         initService.run(url);
     }
+=======
+>>>>>>> parent of 25f3ec5 (Class WordStatisticsHtmlApplication)
 }
--- src/main/java/ru/geraskindenis/word_statistics_html/service/impl/InitServiceImpl.java
+++ src/main/java/ru/geraskindenis/word_statistics_html/service/impl/InitServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.geraskindenis.word_statistics_html.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.geraskindenis.word_statistics_html.service.InitService;
 import ru.geraskindenis.word_statistics_html.service.RecordService;
 import lombok.RequiredArgsConstructor;
@@ -10,18 +11,25 @@ import ru.geraskindenis.word_statistics_html.util.UtilStatistics;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class InitServiceImpl implements InitService {
 
     private final RecordService recordService;
 
     @Override
     public void run(String url) {
+        log.info("Getting text from a HTML-page: " + url);
         var text = HtmlParser.getText(url);
         if (text == null) {
             return;
         }
+        log.info("Text received successfully!");
+
         var regex = "[,\"«»()©'\\[\\]:%]|\\s-\\s|\\.(?=\\s)|\\d{2}\\.\\d{2}\\.\\d{4}|(?<=\\s)\\d+(?=\\s)|\\d-\\d{3}-\\d{3}-\\d{4}|\\.$|—";
         text = text.replaceAll(regex, "");
+
+        log.info("Getting statistics of word and write to database...");
+        StringBuilder builder = new StringBuilder("===Start of report===\n");
         UtilStatistics.getNumberOfRepetitions(text)
                 .forEach((word, count) -> {
                     var record = new StatisticsRecord()
@@ -29,7 +37,15 @@ public class InitServiceImpl implements InitService {
                             .setUrl(url)
                             .setWord(word);
                     recordService.save(record);
-                });
 
+                    builder.append(record.getUrl())
+                            .append(" - ")
+                            .append(record.getWord())
+                            .append(" - ")
+                            .append(record.getQuantity())
+                            .append("\n");
+                });
+        log.info("Statistics successfully received and written to the database!");
+        log.info(builder.toString());
     }
 }
